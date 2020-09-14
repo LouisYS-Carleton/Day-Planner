7877438fe5d25889d832e17539fba6b26a78567e
Louis Yacksmith <louisyscarleton@gmail.com>
2020-09-14 19:58:23 -0400

    Added some semantic html and tidied HTML a little more. Was writting 12AM instead of 12PM, fixed red 12PM slot.

--- js/script-redo.js
+++ js/script-redo.js
@@ -4,7 +4,7 @@ let timeSlot = {
     "9 AM": "",
     "10 AM": "",
     "11 AM": "",
-    "12 AM": "",
+    "12 PM": "",
     "1 PM": "",
     "2 PM": "",
     "3 PM": "",
@@ -53,7 +53,7 @@ function getHourNumber(hourString) {
         case "9 AM": return 9;
         case "10 AM": return 10;
         case "11 AM": return 11;
-        case "12 AM": return 12;
+        case "12 PM": return 12;
         case "1 PM": return 13;
         case "2 PM": return 14;
         case "3 PM": return 15;
